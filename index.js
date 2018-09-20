39514cda75042e887b8df17d0edc7b41b6bb67cd
derrick b <dbozich1@gmail.com>
2018-09-20 06:04:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, name){
-  return drivers.filter(d => d === name );
+  return drivers.filter(d => d === name || d.toLowerCase() === name );
 }
