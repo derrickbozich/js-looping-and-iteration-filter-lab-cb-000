224778cdfa160f256c611000d36d1270bf354df4
derrick b <dbozich1@gmail.com>
2018-09-20 06:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file
 function findMatching(drivers, name){
   re = new RegExp(name, "i");
-  return drivers.filter(d => d.match(re) );
+  return drivers.filter(d => d.match(re));
 }
 
 function fuzzyMatch(drivers, subStr){
-  var re = new RegExp(subStr,'ab+');
+  var re = new RegExp(subStr,'^');
   return drivers.filter(d => d.search(subStr)  );
 }
